4c0c206985ec5cb182992795f3b4884e7c9f8ef1
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-12-11 20:20:41 +0800

    Finished Hero Component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react"
+import Hero from "../Components/Hero"
 import Navbar from "../Components/Navbar"
 
 const Home = (): JSX.Element => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
