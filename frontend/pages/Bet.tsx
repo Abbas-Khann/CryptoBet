7eac543c58f501514133afc5134651b4aedfb65a
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-12-13 22:45:27 +0800

    added images

--- frontend/pages/Bet.tsx
+++ frontend/pages/Bet.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
+import Image from "next/image";
+import upward from "../public/img/Upward.png";
+import downward from "../public/img/downward.png";
 
 const Bet = (): JSX.Element => {
 
@@ -25,8 +28,33 @@ const Bet = (): JSX.Element => {
                 className="text-center text-xl pt-14 pb-1 border-b-2 inline px-4"
                 >Predict the Eth Price ↕️ and Win</h1>
                 </div>
-                <div className="">
-                    
+                <div className="p-5 flex flex-col sm:flex-row justify-around items-center">
+                <div 
+                className=
+                "py-10 my-10 mx-10 border-2 rounded-3xl flex items-center justify-centers flex-col w-10/12 bg-gradient-to-b from-[#010232D4] via-[#0102328C] to-[#0771D38A]"
+                >
+                    <h2 className="text-5xl mb-2">Start {" "}<span className="text-[#0771D3]">Betting</span></h2>
+                    <h4 className="text-xl pb-16">Entry Fee:{" "}<span className="text-[#0771D3]">1.1 Matic</span></h4>
+                    <div className="flex">
+                        <div className="pr-10">
+                        <Image width={50} height={50} src={upward} alt={ "" } />
+                        </div>
+                        <Image width={50} height={50} src={downward} alt={ "" } />
+                    </div>
+                </div>
+                <div 
+                className=
+                "py-10 border-2 rounded-3xl flex items-center justify-center flex-col w-10/12 bg-gradient-to-b from-[#010232D4] via-[#0102328C] to-[#0771D38A]"
+                >
+                    <h2 className="text-5xl mb-2">Start {" "}<span className="text-[#0771D3]">Betting</span></h2>
+                    <h4 className="text-xl pb-16">Entry Fee:{" "}<span className="text-[#0771D3]">1.1 Matic</span></h4>
+                    <div className="flex">
+                    <div className="pr-10">
+                        <Image width={50} height={50} src={upward} alt={ "" } />
+                        </div>
+                        <Image width={50} height={50} src={downward} alt={ "" } />
+                    </div>
+                </div>
                 </div>
             </div>
         )
