5a45946f0789ba9f60fbfc030270273322e60649
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-12-21 15:38:01 +0800

    Updated About file

--- frontend/pages/About.tsx
+++ frontend/pages/About.tsx
@@ -11,7 +11,7 @@ const About = (): JSX.Element => {
             <div className="sm:flex items-center justify-between h-[90vh]">
                <div className="p-10 sm:mb-32 sm:w-1/2 sm:p-0 sm:pl-10">
                 <h1 className="text-3xl sm:text-4xl lg:text-5xl 2xl:text-7xl py-10">How CryptoBet <h1 className="text-blue-500 inline">Works 👇</h1></h1>
-                <p className="text-base lg:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo molestiae, atque facere, esse delectus sequi corporis obcaecati, voluptate totam nesciunt repellendus in excepturi quo at. Numquam sint ipsam modi quibusdam nam, voluptatem aspernatur ad ipsa?
+                <p className="text-base lg:text-lg">CryptoBet allows you to deploy a smart contract where you get to choose the timing and the pricing of the bet and everything and later on users can play a game where they will be able to bet on the price of Eth in the time interval chosen by the owner of the contract and based off the correct or wrong guess the player will be rewarded with crypto or lose the crypto they locked in to the contract while entering the game.
                 </p>
                </div>
                <div className="flex items-end justify-center sm:pt-20 mb-10 sm:mb-0">
